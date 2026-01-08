6a77f8b8af00713bc989068614d382388fa8e0b8
letizia iannucci <letizia.iannucci@aalto.fi>
2026-01-08 13:17:39 +0200

    fix port and fixture

--- tests/networks/builders/conftest.py
+++ tests/networks/builders/conftest.py
@@ -1,22 +1,17 @@
 import pytest
 
 
-@pytest.fixture(autouse=True)
-def Network_spec():
+@pytest.fixture
+def _Spec():
+    """Provide a simple `_Spec` class for builder tests.
+
+    Tests call `_Spec(...)` to create lightweight spec objects used by builders.
+    """
+
     class _Spec:
-        def __init__(self, edge_kind="comment"):
-            self.edge_kind = edge_kind
-            self.filters = {}
-
-    return _Spec
-
-
-@pytest.fixture(autouse=True)
-def Weighted_network_spec():
-    class _Spec:
-        def __init__(self, weighting="binary", edge_kind="comment", filters=None):
+        def __init__(self, filters=None, edge_kind="comment", weighting="count"):
+            self.filters = filters or {}
             self.edge_kind = edge_kind
             self.weighting = weighting
-            self.filters = filters or {"left": "account", "right": "hashtag"}
 
     return _Spec
